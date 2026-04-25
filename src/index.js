9e2f1a39142362452ca8fb58af6b2d32738b36e3
ynkrpnk <ankarpenko35@gmail.com>
2026-04-26 01:19:10 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-'use strict';
+"use strict";
+
+function createLogger(prefix) {
+  return function (message) {
+    console.log(prefix + ": " + message);
+  };
+}
+const authLogger = createLogger("AUTH");
+authLogger("User logged in");
+
+const createLimiter = (limit) => {
+  if (typeof limit !== "number" || Number.isNaN(limit)) {
+    return null;
+  }
+
+  let count = 0;
+
+  return () => {
+    if (count < limit) {
+      count++;
+      return "Ok";
+    } else {
+      return "Error";
+    }
+  };
+};
+
+const limited = createLimiter(2);
+
+console.log(limited());
+console.log(limited());
+console.log(limited());
